5792a0abdc1375fceea15807e329907068c21035
msga-mmm <michael.sga.mmm@gmail.com>
2025-08-10 14:53:23 -0500

    feat: add task done field

--- backend/tasks/models.py
+++ backend/tasks/models.py
@@ -4,3 +4,4 @@ from django.db import models
 class Task(models.Model):
     title = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
+    done = models.BooleanField(default=False)
